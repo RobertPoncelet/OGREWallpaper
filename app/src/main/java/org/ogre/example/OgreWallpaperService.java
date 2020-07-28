4836669fc1b6abefa086089503ebb9df99d1c3f2
RobertPoncelet <robert.poncelet@talk21.com>
2020-07-28 03:01:05 +0100

    OGRE wallpaper works! Crashes sometimes due to OGRE being init twice though

--- app/src/main/java/org/ogre/example/OgreWallpaperService.java
+++ app/src/main/java/org/ogre/example/OgreWallpaperService.java
@@ -1,17 +1,25 @@
 package org.ogre.example;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.content.SharedPreferences;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
+import android.content.res.AssetManager;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.service.wallpaper.WallpaperService;
 import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.SurfaceHolder;
+
+import org.ogre.AnimationState;
+import org.ogre.ApplicationContext;
+import org.ogre.Camera;
+import org.ogre.ColourValue;
+import org.ogre.Entity;
+import org.ogre.Light;
+import org.ogre.Radian;
+import org.ogre.SceneManager;
+import org.ogre.SceneNode;
+import org.ogre.ShaderGenerator;
+import org.ogre.Viewport;
 
 public class OgreWallpaperService extends WallpaperService {
     @Override
@@ -20,33 +28,10 @@ public class OgreWallpaperService extends WallpaperService {
     }
 
     private class OgreWallpaperEngine extends Engine {
-        private final Handler handler = new Handler();
-        private final Runnable drawRunner = new Runnable() {
-            @Override
-            public void run() {
-                draw();
-            }
-
-        };
-        private List<MyPoint> circles;
-        private Paint paint = new Paint();
         private int width;
         int height;
-        private boolean visible = true;
         private int maxNumber;
         private boolean touchEnabled;
-
-        public class MyPoint {
-            String text;
-            private int x;
-            private int y;
-
-            public MyPoint(String text, int x, int y) {
-                this.text = text;
-                this.x = x;
-                this.y = y;
-            }
-        }
 
         public OgreWallpaperEngine() {
             SharedPreferences prefs = PreferenceManager
@@ -54,30 +39,34 @@ public class OgreWallpaperService extends WallpaperService {
             maxNumber = Integer
                     .valueOf(prefs.getString("numberOfCircles", "4"));
             touchEnabled = prefs.getBoolean("touch", false);
-            circles = new ArrayList<MyPoint>();
-            paint.setAntiAlias(true);
-            paint.setColor(Color.WHITE);
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setStrokeJoin(Paint.Join.ROUND);
-            paint.setStrokeWidth(10f);
-            handler.post(drawRunner);
+            //handler.post(drawRunner);
         }
 
         @Override
         public void onVisibilityChanged(boolean visible) {
-            this.visible = visible;
+            paused = !visible;
             if (visible) {
-                handler.post(drawRunner);
+                handler.post(renderer);
             } else {
-                handler.removeCallbacks(drawRunner);
+                handler.removeCallbacks(renderer);
             }
         }
 
         @Override
         public void onSurfaceDestroyed(SurfaceHolder holder) {
             super.onSurfaceDestroyed(holder);
-            this.visible = false;
-            handler.removeCallbacks(drawRunner);
+            this.paused = true;
+            handler.removeCallbacks(renderer);
+
+            if (initOGRE && wndCreate) {
+                wndCreate = false;
+                lastSurface = null;
+                handler.post(new Runnable() {
+                    public void run() {
+                        ogreApp.getRenderWindow()._notifySurfaceDestroyed();
+                    }
+                });
+            }
         }
 
         @Override
@@ -91,164 +80,174 @@ public class OgreWallpaperService extends WallpaperService {
         @Override
         public void onTouchEvent(MotionEvent event) {
             if (touchEnabled) {
-
-                float x = event.getX();
-                float y = event.getY();
-                SurfaceHolder holder = getSurfaceHolder();
-                Canvas canvas = null;
-                try {
-                    canvas = holder.lockCanvas();
-                    if (canvas != null) {
-                        canvas.drawColor(Color.BLACK);
-                        circles.clear();
-                        circles.add(new MyPoint(
-                                String.valueOf(circles.size() + 1), (int)x, (int)y));
-                        drawCircles(canvas, circles);
-
-                    }
-                } finally {
-                    if (canvas != null)
-                        holder.unlockCanvasAndPost(canvas);
-                }
                 super.onTouchEvent(event);
             }
         }
 
-        private void draw() {
-            SurfaceHolder holder = getSurfaceHolder();
-            Canvas canvas = null;
-            try {
-                canvas = holder.lockCanvas();
-                if (canvas != null) {
-                    if (circles.size() >= maxNumber) {
-                        circles.clear();
-                    }
-                    int x = (int) (width * Math.random());
-                    int y = (int) (height * Math.random());
-                    circles.add(new MyPoint(String.valueOf(circles.size() + 1),
-                            x, y));
-                    drawCircles(canvas, circles);
-                }
-            } finally {
-                if (canvas != null)
-                    holder.unlockCanvasAndPost(canvas);
-            }
-            handler.removeCallbacks(drawRunner);
-            if (visible) {
-                handler.postDelayed(drawRunner, 5000);
-            }
-        }
 
-        // Surface view requires that all elements are drawn completely
-        private void drawCircles(Canvas canvas, List<MyPoint> circles) {
-            canvas.drawColor(Color.BLACK);
-            for (MyPoint point : circles) {
-                canvas.drawCircle(point.x, point.y, 20.0f, paint);
-            }
-        }
-    }
 
-    /*public OgreWallpaperService() {
-    }
+        // End Vogella example wallpaper, start OGRE one
 
-    private Renderer mRenderer;
+        protected Handler handler = null;
+        protected Surface lastSurface = null;
 
-    @Override
-    public Engine onCreateEngine() {
-        return new OgreEngine(getBaseContext(), mRenderer);
-    }
+        private Runnable renderer = null;
+        private boolean paused = false;
+        private boolean initOGRE = false;
+        private AssetManager assetMgr = null;
 
-    private class OgreEngine extends Engine {
-        protected Context mContext;
-        protected Renderer mRenderer;
-        protected WallpaperSurfaceView mSurfaceView;
-        protected float mDefaultPreviewOffsetX;
+        ApplicationContext ogreApp = null;
+        SceneManager scnMgr = null;
 
-        class WallpaperSurfaceView extends GLSurfaceView {
-
-            WallpaperSurfaceView(Context context) {
-                super(context);
-            }
-
-            @Override
-            public SurfaceHolder getHolder() {
-                return getSurfaceHolder();
-            }
-
-            public void onDestroy() {
-                super.onDetachedFromWindow();
-            }
-        }
-
-        public OgreEngine(Context context, Renderer renderer) {
-            mContext = context;
-            mRenderer = renderer;
-            mDefaultPreviewOffsetX = 0.5f;
-        }
-
-        @Override
-        public void onOffsetsChanged(float xOffset, float yOffset, float xOffsetStep, float yOffsetStep,
-                                     int xPixelOffset, int yPixelOffset) {
-            super.onOffsetsChanged(xOffset, yOffset, xOffsetStep, yOffsetStep, xPixelOffset, yPixelOffset);
-            if (mRenderer != null) {
-                if (isPreview() && enableDefaultXOffsetInPreview())
-                    xOffset = mDefaultPreviewOffsetX;
-
-                mRenderer.onOffsetsChanged(xOffset, yOffset, xOffsetStep, yOffsetStep, xPixelOffset, yPixelOffset);
-            }
-        }
-
-        public boolean enableDefaultXOffsetInPreview() {
-            return true;
-        }
-
-        @Override
-        public void onTouchEvent(MotionEvent event) {
-            super.onTouchEvent(event);
-            if (mRenderer != null)
-                mRenderer.onTouchEvent(event);
-        }
-
-        @Override
-        @TargetApi(15)
-        public void setOffsetNotificationsEnabled(boolean enabled) {
-            if (Build.VERSION.SDK_INT >= 15) {
-                super.setOffsetNotificationsEnabled(enabled);
-            }
-        }
+        AnimationState animationState;
+        long time;
 
         @Override
         public void onCreate(SurfaceHolder holder) {
             super.onCreate(holder);
-            mSurfaceView = new WallpaperSurfaceView(mContext);
-            //mSurfaceView.setEGLContextClientVersion(Capabilities.getGLESMajorVersion());
-            //mSurfaceView.setRenderMode(ISurface.RENDERMODE_WHEN_DIRTY);
-            mSurfaceView.setSurfaceRenderer(mRenderer);
             setTouchEventsEnabled(true);
-        }
-
-        @Override
-        public void onSurfaceDestroyed(SurfaceHolder holder) {
-            super.onSurfaceDestroyed(holder);
+            handler = new Handler();
+            sysInit();
         }
 
         @Override
         public void onDestroy() {
-            setTouchEventsEnabled(false);
-            mRenderer.onRenderSurfaceDestroyed(null);
-            mRenderer = null;
-            mSurfaceView.onDestroy();
             super.onDestroy();
+
+            Runnable destroyer = new Runnable() {
+                public void run() {
+                    ogreApp.shutdown();
+                }
+            };
+            handler.post(destroyer);
         }
 
-        @Override
-        public void onVisibilityChanged(boolean visible) {
-            super.onVisibilityChanged(visible);
-            if (visible) {
-                mSurfaceView.onResume();
-            } else {
-                mSurfaceView.onPause();
-            }
+        private void sysInit() {
+            final Runnable initRunnable = new Runnable() {
+                public void run() {
+                    if (!initOGRE) {
+                        initOGRE = true;
+
+                        if(assetMgr == null) {
+                            assetMgr = getResources().getAssets();
+                        }
+
+                        renderer = new Runnable() {
+                            public void run() {
+
+                                if (paused)
+                                    return;
+
+                                if (!wndCreate && lastSurface != null) {
+                                    wndCreate = true;
+
+                                    if(ogreApp == null) {
+                                        ogreApp = new ApplicationContext();
+                                        ogreApp.initAppForAndroid(assetMgr, lastSurface);
+
+                                        scnMgr = ogreApp.getRoot().createSceneManager();
+                                        ShaderGenerator.getSingleton().addSceneManager(scnMgr);
+
+                                        scnMgr.setAmbientLight(new ColourValue(0.5f, 0.5f, 0.5f));
+
+                                        Light light = scnMgr.createLight("MainLight");
+                                        SceneNode lightnode = scnMgr.getRootSceneNode().createChildSceneNode();
+                                        lightnode.setPosition(0, 10, 10);
+                                        lightnode.attachObject(light);
+
+                                        Camera cam = scnMgr.createCamera("myCam");
+                                        cam.setNearClipDistance(0.5f);
+                                        cam.setFOVy(new Radian(0.75f));
+                                        cam.setAutoAspectRatio(true);
+
+                                        SceneNode camnode = scnMgr.getRootSceneNode().createChildSceneNode();
+                                        camnode.attachObject(cam);
+                                        camnode.setPosition(0f, 2f, 14);
+                                        camnode.setDirection(0f, -2f, -14f);
+
+                                        Entity ent = scnMgr.createEntity("Cylinder.mesh");
+                                        animationState = ent.getAnimationState("idle");
+                                        animationState.setLoop(true);
+                                        animationState.setEnabled(true);
+                                        time = System.currentTimeMillis();
+                                        SceneNode node = scnMgr.getRootSceneNode().createChildSceneNode("Sire");
+                                        node.attachObject(ent);
+                                        node.scale(0.1f, 0.1f, 0.1f);
+                                        node.translate(0f, -4.2f, 0f);
+                                        //node.rotate(new Vector3(1f, 0f, 0f), new Radian(-1.5f));
+                                        //node.setVisible(false);
+
+                                        Entity ent2 = scnMgr.createEntity("Plane.mesh");
+                                        SceneNode node2 = scnMgr.getRootSceneNode().createChildSceneNode("Plane");
+                                        node2.attachObject(ent2);
+                                        node2.translate(0f, -4.4f, 0f);
+                                        node2.scale(3f, 3f, 3f);
+                                        //node2.setVisible(false);
+
+                                        Viewport vp = ogreApp.getRenderWindow().addViewport(cam);
+                                        vp.setBackgroundColour(new ColourValue(0.3f, 0.3f, 0.3f));
+                                    } else {
+                                        ogreApp.getRenderWindow()._notifySurfaceCreated(lastSurface);
+                                    }
+
+                                    handler.post(this);
+                                    return;
+                                }
+
+                                if (initOGRE && wndCreate) {
+                                    long now = System.currentTimeMillis();
+                                    float delta = (float)(now - time) / 1000f;
+                                    time = now;
+                                    SceneNode fluff = scnMgr.getSceneNode("Sire");
+                                    fluff.yaw(new Radian(delta*0.5f));
+                                    SceneNode cube = scnMgr.getSceneNode("Plane");
+                                    cube.yaw(new Radian(delta*0.5f));
+                                    animationState.addTime(delta);
+                                    ogreApp.getRoot().renderOneFrame();
+                                }
+
+                                handler.post(this);
+                            }
+                        };
+
+                        handler.post(renderer);
+                    }
+                }
+
+            };
+
+            getSurfaceHolder().addCallback(new SurfaceHolder.Callback() {
+                public void surfaceCreated(SurfaceHolder holder) {
+                    if (holder.getSurface() != null
+                            && holder.getSurface().isValid()) {
+                        lastSurface = holder.getSurface();
+                        handler.post(initRunnable);
+                    }
+                }
+
+                public void surfaceDestroyed(SurfaceHolder holder) {
+                    if (initOGRE && wndCreate) {
+                        wndCreate = false;
+                        lastSurface = null;
+                        handler.post(new Runnable() {
+                            public void run() {
+                                ogreApp.getRenderWindow()._notifySurfaceDestroyed();
+                            }
+                        });
+                    }
+                }
+
+                public void surfaceChanged(SurfaceHolder holder, int format,
+                                           int width, int height) {
+
+                }
+            });
         }
-    }*/
+
+        boolean wndCreate = false;
+    }
+
+    static {
+        System.loadLibrary("OgreJNI");
+    }
 }
