18a86183f57d8d1d53bea98b9febcd919ca109fd
RobertPoncelet <robert.poncelet@talk21.com>
2020-07-28 21:54:12 +0100

    Some minor refactoring (no behaviour changes) before I make some major rearrangements

--- app/src/main/java/org/ogre/example/OgreWallpaperService.java
+++ app/src/main/java/org/ogre/example/OgreWallpaperService.java
@@ -5,6 +5,7 @@ import android.content.res.AssetManager;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.service.wallpaper.WallpaperService;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceHolder;
@@ -22,28 +23,44 @@ import org.ogre.ShaderGenerator;
 import org.ogre.Viewport;
 
 public class OgreWallpaperService extends WallpaperService {
+    private final String LOG_TAG = "OgreWallpaperService";
+
+    @Override
+    public void onCreate() {
+        Log.d(LOG_TAG, "onCreate");
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(LOG_TAG, "onDestroy");
+        super.onDestroy();
+    }
+
     @Override
     public Engine onCreateEngine() {
         return new OgreWallpaperEngine();
     }
 
+    // Has to be static so the singleton doesn't freak out if another Engine is created
+    static ApplicationContext ogreApp = null;
+
     private class OgreWallpaperEngine extends Engine {
+        public final String LOG_TAG = "OgreWallpaperEngine";
         private int width;
         int height;
-        private int maxNumber;
         private boolean touchEnabled;
 
         public OgreWallpaperEngine() {
+            Log.d(LOG_TAG, "Constructor");
             SharedPreferences prefs = PreferenceManager
                     .getDefaultSharedPreferences(OgreWallpaperService.this);
-            maxNumber = Integer
-                    .valueOf(prefs.getString("numberOfCircles", "4"));
             touchEnabled = prefs.getBoolean("touch", false);
-            //handler.post(drawRunner);
         }
 
         @Override
         public void onVisibilityChanged(boolean visible) {
+            Log.d(LOG_TAG, "onVisibilityChanged");
             paused = !visible;
             if (visible) {
                 handler.post(renderer);
@@ -54,16 +71,18 @@ public class OgreWallpaperService extends WallpaperService {
 
         @Override
         public void onSurfaceDestroyed(SurfaceHolder holder) {
+            Log.d(LOG_TAG, "onSurfaceDestroyed");
             super.onSurfaceDestroyed(holder);
             this.paused = true;
             handler.removeCallbacks(renderer);
 
             if (initOGRE && wndCreate) {
                 wndCreate = false;
-                lastSurface = null;
+                mLastSurface = null;
                 handler.post(new Runnable() {
                     public void run() {
                         ogreApp.getRenderWindow()._notifySurfaceDestroyed();
+                        ogreApp.shutdown();
                     }
                 });
             }
@@ -72,6 +91,7 @@ public class OgreWallpaperService extends WallpaperService {
         @Override
         public void onSurfaceChanged(SurfaceHolder holder, int format,
                                      int width, int height) {
+            Log.d(LOG_TAG, "onSurfaceChanged");
             this.width = width;
             this.height = height;
             super.onSurfaceChanged(holder, format, width, height);
@@ -89,21 +109,106 @@ public class OgreWallpaperService extends WallpaperService {
         // End Vogella example wallpaper, start OGRE one
 
         protected Handler handler = null;
-        protected Surface lastSurface = null;
-
-        private Runnable renderer = null;
+        protected Surface mLastSurface = null;
         private boolean paused = false;
         private boolean initOGRE = false;
-        private AssetManager assetMgr = null;
+        private AssetManager mAssetMgr = null;
 
-        ApplicationContext ogreApp = null;
         SceneManager scnMgr = null;
 
         AnimationState animationState;
         long time;
 
+        private void createOgreApp(AssetManager assetMgr, Surface lastSurface) {
+            ogreApp = new ApplicationContext();
+            ogreApp.initAppForAndroid(assetMgr, lastSurface);
+
+            scnMgr = ogreApp.getRoot().createSceneManager();
+            ShaderGenerator.getSingleton().addSceneManager(scnMgr);
+
+            scnMgr.setAmbientLight(new ColourValue(0.5f, 0.5f, 0.5f));
+
+            Light light = scnMgr.createLight("MainLight");
+            SceneNode lightnode = scnMgr.getRootSceneNode().createChildSceneNode();
+            lightnode.setPosition(0, 10, 10);
+            lightnode.attachObject(light);
+
+            Camera cam = scnMgr.createCamera("myCam");
+            cam.setNearClipDistance(0.5f);
+            cam.setFOVy(new Radian(0.75f));
+            cam.setAutoAspectRatio(true);
+
+            SceneNode camnode = scnMgr.getRootSceneNode().createChildSceneNode();
+            camnode.attachObject(cam);
+            camnode.setPosition(0f, 2f, 14);
+            camnode.setDirection(0f, -2f, -14f);
+
+            Entity ent = scnMgr.createEntity("Cylinder.mesh");
+            animationState = ent.getAnimationState("idle");
+            animationState.setLoop(true);
+            animationState.setEnabled(true);
+            time = System.currentTimeMillis();
+            SceneNode node = scnMgr.getRootSceneNode().createChildSceneNode("Sire");
+            node.attachObject(ent);
+            node.scale(0.1f, 0.1f, 0.1f);
+            node.translate(0f, -4.2f, 0f);
+            //node.rotate(new Vector3(1f, 0f, 0f), new Radian(-1.5f));
+            //node.setVisible(false);
+
+            Entity ent2 = scnMgr.createEntity("Plane.mesh");
+            SceneNode node2 = scnMgr.getRootSceneNode().createChildSceneNode("Plane");
+            node2.attachObject(ent2);
+            node2.translate(0f, -4.4f, 0f);
+            node2.scale(3f, 3f, 3f);
+            //node2.setVisible(false);
+
+            Viewport vp = ogreApp.getRenderWindow().addViewport(cam);
+            vp.setBackgroundColour(new ColourValue(0.3f, 0.3f, 0.3f));
+        }
+
+        private void updateScene() {
+            long now = System.currentTimeMillis();
+            float delta = (float)(now - time) / 1000f;
+            time = now;
+            SceneNode fluff = scnMgr.getSceneNode("Sire");
+            fluff.yaw(new Radian(delta*0.5f));
+            SceneNode cube = scnMgr.getSceneNode("Plane");
+            cube.yaw(new Radian(delta*0.5f));
+            animationState.addTime(delta);
+            //Log.d(LOG_TAG, "Rendering frame");
+        }
+
+        private Runnable renderer = new Runnable() {
+            public void run() {
+
+                if (paused)
+                    return;
+
+                if (!wndCreate && mLastSurface != null) {
+                    wndCreate = true;
+
+                    if(ogreApp == null) {
+                        createOgreApp(mAssetMgr, mLastSurface);
+                    } else {
+                        ogreApp.getRenderWindow()._notifySurfaceCreated(mLastSurface);
+                    }
+
+                    handler.post(this);
+                    return;
+                }
+
+                if (initOGRE && wndCreate) {
+                    updateScene();
+                    ogreApp.getRoot().renderOneFrame();
+                }
+
+                handler.post(this);
+            }
+        };
+
         @Override
         public void onCreate(SurfaceHolder holder) {
+            Log.d(LOG_TAG, "onCreate");
             super.onCreate(holder);
             setTouchEventsEnabled(true);
             handler = new Handler();
@@ -112,103 +217,20 @@ public class OgreWallpaperService extends WallpaperService {
 
         @Override
         public void onDestroy() {
+            Log.d(LOG_TAG, "onDestroy");
             super.onDestroy();
-
-            Runnable destroyer = new Runnable() {
-                public void run() {
-                    ogreApp.shutdown();
-                }
-            };
-            handler.post(destroyer);
         }
 
         private void sysInit() {
+            Log.d(LOG_TAG, "sysInit");
             final Runnable initRunnable = new Runnable() {
                 public void run() {
                     if (!initOGRE) {
                         initOGRE = true;
 
-                        if(assetMgr == null) {
-                            assetMgr = getResources().getAssets();
+                        if(mAssetMgr == null) {
+                            mAssetMgr = getResources().getAssets();
                         }
-
-                        renderer = new Runnable() {
-                            public void run() {
-
-                                if (paused)
-                                    return;
-
-                                if (!wndCreate && lastSurface != null) {
-                                    wndCreate = true;
-
-                                    if(ogreApp == null) {
-                                        ogreApp = new ApplicationContext();
-                                        ogreApp.initAppForAndroid(assetMgr, lastSurface);
-
-                                        scnMgr = ogreApp.getRoot().createSceneManager();
-                                        ShaderGenerator.getSingleton().addSceneManager(scnMgr);
-
-                                        scnMgr.setAmbientLight(new ColourValue(0.5f, 0.5f, 0.5f));
-
-                                        Light light = scnMgr.createLight("MainLight");
-                                        SceneNode lightnode = scnMgr.getRootSceneNode().createChildSceneNode();
-                                        lightnode.setPosition(0, 10, 10);
-                                        lightnode.attachObject(light);
-
-                                        Camera cam = scnMgr.createCamera("myCam");
-                                        cam.setNearClipDistance(0.5f);
-                                        cam.setFOVy(new Radian(0.75f));
-                                        cam.setAutoAspectRatio(true);
-
-                                        SceneNode camnode = scnMgr.getRootSceneNode().createChildSceneNode();
-                                        camnode.attachObject(cam);
-                                        camnode.setPosition(0f, 2f, 14);
-                                        camnode.setDirection(0f, -2f, -14f);
-
-                                        Entity ent = scnMgr.createEntity("Cylinder.mesh");
-                                        animationState = ent.getAnimationState("idle");
-                                        animationState.setLoop(true);
-                                        animationState.setEnabled(true);
-                                        time = System.currentTimeMillis();
-                                        SceneNode node = scnMgr.getRootSceneNode().createChildSceneNode("Sire");
-                                        node.attachObject(ent);
-                                        node.scale(0.1f, 0.1f, 0.1f);
-                                        node.translate(0f, -4.2f, 0f);
-                                        //node.rotate(new Vector3(1f, 0f, 0f), new Radian(-1.5f));
-                                        //node.setVisible(false);
-
-                                        Entity ent2 = scnMgr.createEntity("Plane.mesh");
-                                        SceneNode node2 = scnMgr.getRootSceneNode().createChildSceneNode("Plane");
-                                        node2.attachObject(ent2);
-                                        node2.translate(0f, -4.4f, 0f);
-                                        node2.scale(3f, 3f, 3f);
-                                        //node2.setVisible(false);
-
-                                        Viewport vp = ogreApp.getRenderWindow().addViewport(cam);
-                                        vp.setBackgroundColour(new ColourValue(0.3f, 0.3f, 0.3f));
-                                    } else {
-                                        ogreApp.getRenderWindow()._notifySurfaceCreated(lastSurface);
-                                    }
-
-                                    handler.post(this);
-                                    return;
-                                }
-
-                                if (initOGRE && wndCreate) {
-                                    long now = System.currentTimeMillis();
-                                    float delta = (float)(now - time) / 1000f;
-                                    time = now;
-                                    SceneNode fluff = scnMgr.getSceneNode("Sire");
-                                    fluff.yaw(new Radian(delta*0.5f));
-                                    SceneNode cube = scnMgr.getSceneNode("Plane");
-                                    cube.yaw(new Radian(delta*0.5f));
-                                    animationState.addTime(delta);
-                                    ogreApp.getRoot().renderOneFrame();
-                                }
-
-                                handler.post(this);
-                            }
-                        };
 
                         handler.post(renderer);
                     }
@@ -218,17 +240,19 @@ public class OgreWallpaperService extends WallpaperService {
 
             getSurfaceHolder().addCallback(new SurfaceHolder.Callback() {
                 public void surfaceCreated(SurfaceHolder holder) {
+                    Log.d(LOG_TAG, "surfaceCreated");
                     if (holder.getSurface() != null
                             && holder.getSurface().isValid()) {
-                        lastSurface = holder.getSurface();
+                        mLastSurface = holder.getSurface();
                         handler.post(initRunnable);
                     }
                 }
 
                 public void surfaceDestroyed(SurfaceHolder holder) {
+                    Log.d(LOG_TAG, "surfaceDestroyed");
                     if (initOGRE && wndCreate) {
                         wndCreate = false;
-                        lastSurface = null;
+                        mLastSurface = null;
                         handler.post(new Runnable() {
                             public void run() {
                                 ogreApp.getRenderWindow()._notifySurfaceDestroyed();
@@ -239,7 +263,7 @@ public class OgreWallpaperService extends WallpaperService {
 
                 public void surfaceChanged(SurfaceHolder holder, int format,
                                            int width, int height) {
-
+                    Log.d(LOG_TAG, "surfaceChanged");
                 }
             });
         }
